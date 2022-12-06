fd4c8c0de366390ab05f79c46ed5af1892710b93
yuo <1573912618@qq.com>
2022-12-06 18:06:52 +0800

    0.03 test_version in 2022/12/6

--- src/test/java/edu/wfit/hotelmanage/HotelManageApplicationTests.java
+++ src/test/java/edu/wfit/hotelmanage/HotelManageApplicationTests.java
@@ -1,13 +1,18 @@
 package edu.wfit.hotelmanage;
 
+import edu.wfit.hotelmanage.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HotelManageApplicationTests {
 
+    @Autowired
+    UserService userService;
     @Test
     void contextLoads() {
+        long count = userService.count();
     }
 
 }
